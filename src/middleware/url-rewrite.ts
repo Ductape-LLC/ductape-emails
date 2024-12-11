fc8e7c75ce30e9159c57820bda0ac8718ce4d46b
Christopher Wuni <christdam55@gmail.com>
2024-12-11 12:20:46 +0100

    updated env and url-rewrite

--- src/middleware/url-rewrite.ts
+++ src/middleware/url-rewrite.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const urlRewrite = async (req: Request, _: Response, next: NextFunction) => {
-  if (req.url.startsWith('/emails')) {
+  if (req.url.startsWith('/emails') && !req.url.startsWith('/emails/v1')) {
     req.url = req.url.replace('/emails', '');
   }
   next();
